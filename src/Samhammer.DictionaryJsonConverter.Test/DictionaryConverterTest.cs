99cf5e1de8f7d114859a9ef30fac28de99878d76
Roland Schmitt <rschmitt@samhammer.de>
2019-04-29 16:21:27 +0200

    Added code from stackoverflow: https://stackoverflow.com/questions/5546142/how-do-i-use-json-net-to-deserialize-into-nested-recursive-dictionary-and-list/38029052#38029052

---
+++ src/Samhammer.DictionaryJsonConverter.Test/DictionaryConverterTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Samhammer.DictionaryJsonConverter.Test
+{
+    public class DictionaryConverterTest
+    {
+        [Fact]
+        public void ValuesAreDeserialized()
+        {
+            var json = JsonConvert.SerializeObject(new
+            {
+                Flag = true,
+                Number = 1234,
+                Date = new DateTime(2018, 12, 31),
+            });
+
+            var expected = new Dictionary<string, object>()
+            {
+                { "Flag", true },
+                { "Number", 1234 },
+                { "Date", new DateTime(2018, 12, 31) },
+            };
+
+            var result = JsonConvert.DeserializeObject<IDictionary<string, object>>(json, new DictionaryConverter());
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ObjectsAreDeserialized()
+        {
+            var json = JsonConvert.SerializeObject(new
+            {
+                Child = new
+                {
+                    Name = "test",
+                },
+            });
+
+            var expected = new Dictionary<string, object>()
+            {
+                {
+                    "Child", new Dictionary<string, object>()
+                    {
+                        { "Name", "test" },
+                    }
+                },
+            };
+
+            var result = JsonConvert.DeserializeObject<IDictionary<string, object>>(json, new DictionaryConverter());
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ListsAreDeserialized()
+        {
+            var json = JsonConvert.SerializeObject(new
+            {
+                List = new List<object>
+                {
+                    new { Value = "abc" },
+                    new { Value = "def" },
+                },
+            });
+
+            var expected = new Dictionary<string, object>()
+            {
+                {
+                    "List", new List<object>
+                    {
+                        new Dictionary<string, object>() { { "Value", "abc" } },
+                        new Dictionary<string, object>() { { "Value", "def" } },
+                    }
+                },
+            };
+
+            var result = JsonConvert.DeserializeObject<IDictionary<string, object>>(json, new DictionaryConverter());
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
